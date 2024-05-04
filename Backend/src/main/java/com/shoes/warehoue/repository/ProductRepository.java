cf11df95283f6ce562690eac0e4cd25193535b80
Brandy <31572555+DuythuanBK@users.noreply.github.com>
2024-05-04 14:23:05 +0700

    fix home doesn't display products

--- Backend/src/main/java/com/shoes/warehoue/repository/ProductRepository.java
+++ Backend/src/main/java/com/shoes/warehoue/repository/ProductRepository.java
@@ -32,5 +32,17 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
             "  \tleft join stock s on p.code = s.product_code\n" +
             "\t  group by  p.code ) p2 on p.code = p2.code\n" +
             "where p.code like %:productCode%", nativeQuery = true)
-    Page<ProductEntity> findProductQuantity(@Param("productCode") String productCode, Pageable pageable);
+    Page<ProductEntity> findProductQuantityLikeCode(@Param("productCode") String productCode, Pageable pageable);
+
+    @Query( value = "select p.id, p.code, p.image, p.note from product p \n" +
+            "join\n" +
+            "  (select p.code from product p\n" +
+            "    left join imports i on p.code = i.product_code\n" +
+            "    where i.status in ('Đã đặt hàng', 'Hàng đang về')\n" +
+            "\t  group by  p.code \n" +
+            "\tunion \n" +
+            "\t  select p.code from product p\n" +
+            "  \tleft join stock s on p.code = s.product_code\n" +
+            "\t  group by  p.code ) p2 on p.code = p2.code\n", nativeQuery = true)
+    Page<ProductEntity> findProductQuantity(Pageable pageable);
 }
